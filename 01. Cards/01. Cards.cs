08fb79e5f052761fe5d59b70dd8a04300ac97ce9
warglaivee <warglaivegm@gmail.com>
2017-04-20 23:32:03 +0300

    01.Cards Written PC
    02.Main logic commented, to be made soon

---
+++ 01. Cards/01. Cards.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+namespace _01.Cards
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var result = new List<string>();
+            var input = Console.ReadLine();
+            var pattern = @"([1]*[0-9JQKA])([SHDC])";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(input);
+            foreach (Match card in matches)
+            {
+                var power = 0;
+                if (int.TryParse(card.Groups[1].Value, out power))
+                {
+                    if (power < 2 || power > 10)
+                    {
+                        continue;
+                    }
+                }
+                result.Add(card.Value);
+            }
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
---
+++ 02. Fish Statistics/02. Fish Statistics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace _02.Fish_Statistics
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            var bodyPartsCount = new Dictionary<string, int>(); // count char occurances
+            var matchResult = new List<string>(); // add legit fishes here 
+            var input = Console.ReadLine();
+            var pattern = @"[>|<]*<*\(+['|x|-]>";
+            var regex = new Regex(pattern);
+
+            var matches = regex.Matches(input);
+            foreach (var fish in matches)
+            {
+                matchResult.Add(fish.ToString());
+            }
+            foreach (var fish in matchResult)
+            {
+                foreach (var fishParts in fish)
+                {
+                    if (fishParts < 5) //use dictionary value to check character's count
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
