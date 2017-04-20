61c8dd147da4e861f3598f2ef6aac58a890a063b
warglaivee <warglaivegm@gmail.com>
2017-04-20 19:12:18 +0300

    Lab done

---
+++ 01.Match Full Name/MatchFullNameLAB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace _01.Match_Full_Name
+{
+    public class MatchFullNameLAB
+    {
+       public static void Main()
+        {
+            var input = Console.ReadLine();
+            while (input!="end")
+            {
+                var pattern= @"<a.+?href=(.+?)>(.+?)<\/a>";
+                var regex = new Regex(pattern);
+                var replacements = "[URL href=$1]$2[/URL]";
+                var result = regex.Replace(input, replacements);
+                Console.WriteLine(result);
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
