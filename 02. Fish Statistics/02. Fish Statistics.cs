2d42d2ffef87154a534b380cfbab33d24c049271
warglaivee <warglaivegm@gmail.com>
2017-04-21 18:46:05 +0300

    Written PC

--- 02. Fish Statistics/02. Fish Statistics.cs
+++ 02. Fish Statistics/02. Fish Statistics.cs
@@ -1,32 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 namespace _02.Fish_Statistics
 {
-   public class Program
+    public class Program
     {
-       public static void Main()
+        public static void Main()
         {
-            var bodyPartsCount = new Dictionary<string, int>(); // count char occurances
+            var bodyPartsCount = new Dictionary<string, long>(); // count char occurances
             var matchResult = new List<string>(); // add legit fishes here 
             var input = Console.ReadLine();
-            var pattern = @"([>]*)<*(\(+)(['|x|-])>";
+            var pattern = @"(>*)<(\(+)([\'-]|x)>";
             var regex = new Regex(pattern);
-            var tailLenghtSantimeters = 0;
+            long tailLenghtSantimeters = 0;
+            long bodyLenghtSantimeters = 0;
             var tailType = string.Empty;
-
+            var bodyType = string.Empty;
+            var confirmStatus = string.Empty;
             var matches = regex.Matches(input);
+            if (matches.Count < 1)
+            {
+                Console.WriteLine("No fish found.");
+            }
+            long counter = 0;
             foreach (Match fish in matches)
             {
+                counter++;
                 matchResult.Add(fish.ToString());
-                var tailLenght = fish.Groups[1].Length;
-                var bodyLenght = fish.Groups[2].Length;
+                long tailLenght = fish.Groups[1].Length;
+                long bodyLenght = fish.Groups[2].Length;
+                var status = fish.Groups[3].ToString();
                 tailLenghtSantimeters = tailLenght * 2; //tail to CM
-
-                if (tailLenghtSantimeters > 10)
+                bodyLenghtSantimeters = bodyLenght * 2;
+                if (tailLenghtSantimeters > 10) //tail
                 {
                     tailType = "Long";
                 }
@@ -42,7 +48,41 @@ namespace _02.Fish_Statistics
                 {
                     tailType = "None";
                 }
-
+                if (bodyLenghtSantimeters > 20) //body
+                {
+                    bodyType = "Long";
+                }
+                else if (bodyLenghtSantimeters > 10)
+                {
+                    bodyType = "Medium";
+                }
+                else if (bodyLenghtSantimeters >= 2 && bodyLenghtSantimeters <= 8)
+                {
+                    bodyType = "Short";
+                }
+                if (status == "'") //status
+                {
+                    confirmStatus = "Awake";
+                }
+                else if (status == "-")
+                {
+                    confirmStatus = "Asleep";
+                }
+                else if (status == "x")
+                {
+                    confirmStatus = "Dead";
+                }
+                Console.WriteLine($"Fish {counter}: {fish}");
+                if (tailLenghtSantimeters >= 2)
+                {
+                    Console.WriteLine($"  Tail type: {tailType} ({tailLenghtSantimeters} cm)");
+                }
+                else
+                {
+                    Console.WriteLine($"  Tail type: {tailType}");
+                }
+                Console.WriteLine($"  Body type: {bodyType} ({bodyLenghtSantimeters} cm)");
+                Console.WriteLine($"  Status: {confirmStatus}");
             }
         }
     }
