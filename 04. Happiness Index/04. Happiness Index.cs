9bcef992eb1e3bc982909d4c29e99db63536883a
warglaivee <warglaivegm@gmail.com>
2017-04-21 21:28:24 +0300

    UP from PC

---
+++ 04. Happiness Index/04. Happiness Index.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _04_HappinessIndex
+{
+    public class HappinessIndex
+    {
+        public static void Main()
+        {
+            var HappyPattern = @":\)|:D|;\)|:\*|:]|;]|:]|:}|;}|\(:|\*:|c:|\[:|\[;";
+            var SadPattern = @":\(|D:|;\(|:\[|;\[|:\{|;\{|\):|:c|\]:|\];";
+
+            Regex HappyRegex = new Regex(HappyPattern);
+            Regex SadRegex = new Regex(SadPattern);
+
+            var input = Console.ReadLine();
+            var happyCount = HappyRegex.Matches(input).Count;
+            var sadCount = SadRegex.Matches(input).Count;
+            var HappinessIndex = happyCount / (double)sadCount;
+            var picture = HappinessPicture(HappinessIndex);
+
+            Console.WriteLine($"Happiness index: {HappinessIndex:F2} {picture}");
+            Console.WriteLine($"[Happy count: {happyCount}, Sad count: {sadCount}]");
+        }
+
+        private static string HappinessPicture(double happinessIndex)
+        {
+            if (happinessIndex >= 2)
+            {
+                return ":D";
+            }
+            else if (happinessIndex > 1)
+            {
+                return ":)";
+            }
+            else if (happinessIndex == 1)
+            {
+                return ":|";
+            }
+            else
+            {
+                return ":(";
+            }
+            {
+
+            }
+        }
+    }
+}
